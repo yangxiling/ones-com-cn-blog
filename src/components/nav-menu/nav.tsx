cdb580fafe29e03ab90441e28bccf6e22ff7c838
yangxiling <1335939860@qq.com>
2025-06-25 12:13:42 +0800

    feat: nav trail

--- src/components/nav-menu/nav.tsx
+++ src/components/nav-menu/nav.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import {Button} from '@ones-design/core';
 import styles from './nav.module.scss';
 import onesLogo from '../../assets/onescom.svg';
 import {Menu} from './menu';
 import {URL_CONFIG} from '../../const/url-config';
 
 export const NavMenu = React.memo(() => {
-
-  const handleBtnClick = () => {
-    window.location.href = 'https://ones.cn/service_success?utm_term=%E9%A2%84%E7%BA%A6%E6%BC%94%E7%A4%BA&_channel_track_key=NdBOAxB4&utm_source=ones-issue';
-  };
-
-  const handleTrialClick = () => {
-    window.location.href = 'https://ones.cn/sign_up?utm_source=ones-issue&utm_term=home&utm_campaign=banner&_channel_track_key=KJ4xUv9v';
-  };
 
   return (
     <nav id="nav-container" className={styles.container}>
@@ -26,15 +17,15 @@ export const NavMenu = React.memo(() => {
           <Menu />
         </div>
         <div className={styles.right}>
-          <Button
-            type="default"
-            className={styles.phone}
+          <a
+
+            className={styles.text}
             href={`${URL_CONFIG.baseUrl}/contact_us`}
           >
-            Contact sales
-          </Button>
-          <Button type="link" className={styles.login} href={`${URL_CONFIG.baseUrl}/identity/login`}>Log in</Button>
-          <Button type="primary" className={styles.trial} href={`${URL_CONFIG.baseUrl}/sign_up`}>Try for free</Button>
+            <span>Contact sales</span>
+          </a>
+          <a className={styles.text} href={`${URL_CONFIG.baseUrl}/identity/login`}><span>Log in</span></a>
+          <a href={`${URL_CONFIG.baseUrl}/sign_up`}><button className={styles.trial}>Try for free</button></a>
         </div>
       </div>
     </nav>
